5c8cb75ab1c83c4ae3b99558517bf1a27a0f628c
Dandada <23005784+iamdada@users.noreply.github.com>
2019-04-13 12:17:46 -0400

    playing around with react native element components

--- app/index.js
+++ app/index.js
@@ -6,46 +6,80 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, TextInput, Image } from 'react-native';
 import TextArea from './components/TextArea/TextArea.component';
+import { Button, ThemeProvider, Card, ListItem, Icon } from 'react-native-elements';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android:
+        'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Please Sign In</Text>
-        <TextArea/>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+// type Props = {};
+// export default class App extends Component<Props> {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>Please Sign In</Text>
+//         <TextArea/>
+//         <Text style={styles.instructions}>To get started, edit App.js</Text>
+//         <Text style={styles.instructions}>{instructions}</Text>
+//       </View>
+//     );
+//   }
+// }
+const users = [
+    {
+        name: 'brynn',
+        avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+    },
+
+];
+
+export default class App extends Component {
+    render() {
+        return (
+            <ThemeProvider>
+                <Button title="Hey!" />
+                <Card title="CARD WITH DIVIDER">
+                    {
+                        users.map((u, i) => {
+                            return (
+                                <View key={i}>
+                                    <Image
+                                        resizeMode="cover"
+                                        source={{ uri: u.avatar }}
+                                    />
+                                    <Text >{u.name}</Text>
+                                </View>
+                            );
+                        })
+                    }
+                </Card>
+            </ThemeProvider>
+        );
+    }
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
